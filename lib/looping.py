422f26f5144f93e0f7be4262fd7d80c6ffc2a3ca
Kitui Kelly <kituikelly@gmail.com>
2024-05-19 17:02:53 +0300

    completes lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,37 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+  """Counts down from 10 to 1 and prints "Happy New Year!" at the end."""
+
+  # Start at 10
+  counter = 10
+
+  # Keep counting down until we reach 1
+  while counter >= 1:
+    # Print the current counter
+    print(counter)
+
+    # Decrement the counter by 1
+    counter -= 1
+
+  # Print "Happy New Year!" when we reach 1
+  print("Happy New Year!")
+
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    squared_elements = [element * element for element in int_list]
+    return squared_elements
 
 def fizzbuzz():
-    # code goes here!
-    pass
+  for i in range(1, 101):
+    if i % 3 == 0 and i % 5 == 0:
+      print("FizzBuzz")
+    elif i % 3 == 0:
+      print("Fizz")
+    elif i % 5 == 0:
+      print("Buzz")
+    else:
+      print(i)
+
+
